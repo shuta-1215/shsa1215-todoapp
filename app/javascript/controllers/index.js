6e11e79f1362acfd91f42c80cab2e64b733107ba
SHSA1215 <syuta.1215sato@gmail.com>
2026-02-09 21:08:20 +0900

    エラーメッセージの日本語化、フラッシュメッセージの追加

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,5 +7,8 @@ import { application } from "./application"
 import DropdownController from "./dropdown_controller"
 application.register("dropdown", DropdownController)
 
+import FlashController from "./flash_controller"
+application.register("flash", FlashController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
