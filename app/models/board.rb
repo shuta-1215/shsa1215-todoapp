574c1b3fff4a886813500cefba5f58e6653cdb32
SHSA1215 <syuta.1215sato@gmail.com>
2026-02-09 20:00:39 +0900

    バリデーションの設定

--- app/models/board.rb
+++ app/models/board.rb
@@ -14,5 +14,8 @@
 #  index_boards_on_user_id  (user_id)
 #
 class Board < ApplicationRecord
-  
+  belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :content, presence: true
 end
