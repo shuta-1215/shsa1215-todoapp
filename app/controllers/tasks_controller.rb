9319e8aed07c540eb8e8478298e0834ef7850e4f
SHSA1215 <syuta.1215sato@gmail.com>
2026-02-24 20:34:34 +0900

    Taskにアイキャッチ画像の登録機能を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -35,6 +35,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:title, :content, :deadline)
+    params.require(:task).permit(:title, :content, :deadline, :eyecatch)
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -24,6 +24,7 @@
 class Task < ApplicationRecord
   belongs_to :user
   belongs_to :board
+  has_one_attached :eyecatch
 
   validates :title, presence: true
 end
