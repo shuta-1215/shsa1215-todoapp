7abb4855170c862b93e5f4c006a9cdd4c5673939
SHSA1215 <syuta.1215sato@gmail.com>
2026-03-02 20:15:22 +0900

    編集・削除機能の追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
   before_action :set_board
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def show
     @task = @board.tasks.find(params[:id])
@@ -24,14 +25,25 @@ class TasksController < ApplicationController
   end
 
   def update
+    if @task.update(task_params)
+      redirect_to board_task_path(@board, @task), notice: "タスクを更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @task.destroy
+    redirect_to board_path(@board), notice: "タスクを削除しました", status: :see_other
   end
 
   private
   def set_board
     @board = Board.find(params[:board_id])
+  end
+
+  def set_task
+    @task = @board.tasks.find(params[:id])
   end
 
   def task_params
